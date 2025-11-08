f41b429ba7cd41249f21fc07b74a525ed948a444
Nathan Vogel <ndvogel399@icloud.com>
2025-11-07 15:06:28 -1000

    list contacts page mockup finished in 23 minutes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,61 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import { PeopleFill, FileEarmarkTextFill, Calendar2CheckFill } from 'react-bootstrap-icons';
+// import { prisma } from '@lib/prisma'; // Comment out or remove this line
+// import StuffItem from '@components/StuffItem'; // Comment out or remove this line
+import { Contact } from '@lib/validationSchemas';
+import ContactCard from '@components/ContactCard';
 
-/** The Home page. */
-const Home = () => (
-  <main>
-    <Container id="landing-page" className="py-3">
-      {/* Removed 'fluid' to get margins */}
-      <Row className="align-middle text-center">
-        <Col>
-          {/* No classNames */}
-          <PeopleFill size={100} />
-          <h1>Multiple Users</h1>
-          <h5>
-            This address book enables any number of users to register and save their business
-            contacts. You can only see the contacts you have created.
-          </h5>
-        </Col>
+/** The List Contacts page. */
+const ListContacts = () => {
+  // const owner = 'john@foo.com'; // Comment out or remove this line
+  // const stuff = await prisma.stuff.findMany({ where: { owner }, orderBy: { name: 'asc' } }); // Comment out or remove this line
+  const contacts: Contact[] = [
+    {
+      firstName: 'Philip',
+      lastName: 'Johnson',
+      address: 'POST 307, University of Hawaii',
+      image: 'https://github.com/philipmjohnson.png',
+      description:
+        'I am a Professor of Information and Computer Sciences at the '
+        + 'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
+    },
+    {
+      firstName: 'Henri',
+      lastName: 'Casanova',
+      address: 'POST 307, University of Hawaii',
+      image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
+      description:
+        'I am originally from France. I maintain a list of reports from my surf sessions. I have proof '
+        + 'that I ran the Hana relay with an actual Team.',
+    },
+    {
+      firstName: 'Kim',
+      lastName: 'Binsted',
+      address: 'POST 307, University of Hawaii',
+      image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
+      description:
+        'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence'
+        + 'from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of '
+        + 'punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as '
+        + '"What do you call a Martian who drinks beer? An ale-ien!".',
+    },
+  ];
 
-        <Col>
-          {/* No classNames */}
-          <FileEarmarkTextFill size={100} />
-          <h1>Contact Details</h1>
-          <h5>
-            For each contact, you can save their name, address, and phone number.
-          </h5>
-        </Col>
-
-        <Col>
-          {/* No classNames */}
-          <Calendar2CheckFill size={100} />
-          <h1>Timestamped Notes</h1>
-          <h5>
-            Each time you make contact with a contact, you can write a note that summarizes the
-            conversation. This note is saved along with a timestamp with the contact.
-          </h5>
-        </Col>
-      </Row>
+  return (
+    <Container id="list-contacts-page" className="py-3">
+      <Col>
+        {/* Changed from "List Contacts" to "Contacts" to match image */}
+        <h2>Contacts</h2>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {contacts.map((contact) => ( // Removed index from map for react/no-array-index-key
+            /* Using firstName as key, assuming unique */
+            <Col key={`Contact-${contact.firstName}`}>
+              <ContactCard contact={contact} />
+            </Col>
+          ))}
+        </Row>
+      </Col>
     </Container>
-  </main>
-);
+  );
+};
 
-export default Home;
+export default ListContacts;
---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Card, Image } from 'react-bootstrap';
+import { Contact } from '@lib/validationSchemas';
+
+interface ContactCardProps {
+  contact: Contact;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({ contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width="75px" roundedCircle className="me-2" />
+      <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
